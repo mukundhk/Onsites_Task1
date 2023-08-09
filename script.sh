85aa9489bd6fd427a57ee9b4f752904a53c11001
Mukund Harikumar <74809793+mukundhk@users.noreply.github.com>
2023-08-09 23:09:00 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+tail -n+2 City_tax_Records.txt | grep No > defaulters.txt
+
+echo "Address Tax" > output.txt
+while read address;
+do  
+    incomes=( $(grep -w "$address" defaulters.txt | cut -d" " -f5) )
+    sum=0
+    for i in ${incomes[@]};
+    do
+        sum=$(echo "sum=$sum;sum+=$i;sum" | bc)
+    done
+    tax=$(echo "$sum*0.2" | bc)
+    echo $address $tax >> output.txt
+done < <(cat defaulters.txt | cut -d" " -f2,3,4 | sort | uniq -u && cat defaulters.txt | cut -d" " -f2,3,4 | sort | uniq -d)
+
+rm ./defaulters.txt
